a7096067e379b0f3da0c4176ae51ab4495eacaa1
Felipe <felipeldn91@gmail.com>
2019-05-05 18:47:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function whisper(string) {return string.toLowerCase()}
 function logShout(string) {console.log (string.toUpperCase())}
 function logWhisper(string) {console.log (string.toLowerCase())}
 
+<<<<<<< HEAD
 function sayHiToGrandma(string) {
   if(string.toLowerCase()==string) {
     return "I can't hear you!";
@@ -15,5 +16,11 @@ else if("I love you, Grandma."==string) {
   return "I love you, too.";
   
 }
+=======
+function sayHiToGrandma(string) 
+{if(string.toLowerCase()==string) return "I can't hear you!"
+else if(string.toUpperCase()==string) return "YES INDEED!"
+else if("I love you, Grandma"==string) return "I love you, too."
+>>>>>>> 803d443a6af98fd8e3151d33bf3c704d0ef065e8
   
 }
